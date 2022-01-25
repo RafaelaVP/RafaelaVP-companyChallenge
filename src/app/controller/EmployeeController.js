ebb14282e927908d0276264c46825ee8b3dd5939
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-25 15:15:25 -0300

    feat: create update, delete, get all and getby id methods in controller, gets not working.

--- src/app/controller/EmployeeController.js
+++ src/app/controller/EmployeeController.js
@@ -10,6 +10,45 @@ class EmployeeController {
             
         }
     }
+    async getAll(req, res) {
+        try {
+            const result = await EmployeeService.findAll(req.query)
+            return res.status(200).json(result)
+        } catch (error) {
+            return res.status(400).json(error.message)
+        }
+    }
+    async update(req, res) {
+        try {
+          const { id } = req.params;
+          const update = req.body;
+          const result = await EmployeeService.update(id, update);
+          return res.status(200).json(result);
+        } catch (error) {
+          return res.status(400).json({ message: error.message });
+        }
+      }
+    
+      async delete(req, res) {
+        try {
+          const { id } = req.params;
+          const result = await EmployeeService.delete(id);
+          return res.status(204).json(result);
+        } catch (error) {
+          return res.status(400).json({ message: error.message });
+        }
+      }
+    
+      async getById(req, res) {
+        try {
+          const { _id } = req.params;
+          const result = await EmployeeService.getById(_id);
+          return res.status(200).json(result);
+        } catch (error) {
+          return res.status(400).json(message.error);
+        }
+      }
+    
 }
 
 module.exports = new EmployeeController();
