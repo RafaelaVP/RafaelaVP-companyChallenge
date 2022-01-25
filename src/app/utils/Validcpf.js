d32f157fc11ff40457d783d88ab05754e8487486
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-25 17:32:45 -0300

    feat:created validation of the cpf and for the find the formatting to place the dots and dashes.

---
+++ src/app/utils/Validcpf.js
@@ -0,0 +1,45 @@
+function validCPF(cpf) {
+    const Cpf = cpf.replace(/[.-]/g, '');
+    if (
+      Cpf.length !== 11 ||
+      Cpf === '00000000000' ||
+      Cpf === '11111111111' ||
+      Cpf === '22222222222' ||
+      Cpf === '33333333333' ||
+      Cpf === '44444444444' ||
+      Cpf === '55555555555' ||
+      Cpf === '66666666666' ||
+      Cpf === '77777777777' ||
+      Cpf === '88888888888' ||
+      Cpf === '99999999999'
+    )
+      throw new Error({
+        description: 'Bad Request',
+        name: `Invalid CPF ${cpf}`
+      });
+  
+    let add = 0;
+    let i;
+    let rev;
+    for (i = 0; i < 9; i++) add += parseInt(Cpf.charAt(i), 10) * (10 - i);
+    rev = 11 - (add % 11);
+    if (rev === 10 || rev === 11) rev = 0;
+    if (rev !== parseInt(Cpf.charAt(9), 10))
+      throw new Error({
+        description: 'Bad Request',
+        name: `Invalid CPF ${cpf}`
+      });
+  
+    add = 0;
+    for (i = 0; i < 10; i++) add += parseInt(Cpf.charAt(i), 10) * (11 - i);
+    rev = 11 - (add % 11);
+    if (rev === 10 || rev === 11) rev = 0;
+    if (rev !== parseInt(Cpf.charAt(10), 10))
+      throw new Error({
+        description: 'Bad Request',
+        name: `Invalid CPF ${cpf}`
+      });
+  }
+  
+  module.exports = validCPF;
+  
