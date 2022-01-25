c480ae2e0b361efb47a944eebcf8fc7ec353fe87
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-25 15:17:19 -0300

    reafatory: added new routes and identified the non-functioning of gets.

--- src/app/routes/employeeRoute.js
+++ src/app/routes/employeeRoute.js
@@ -2,5 +2,9 @@ const EmployeeController = require('../controller/EmployeeController');
 
 module.exports = (server, routes, prefix = '/api/v1/employee') => {
   routes.post('/', EmployeeController.create);
+  routes.get('/:id',  EmployeeController.getById);
+  routes.get('/', EmployeeController.getAll);
+  routes.put('/:id',  EmployeeController.update);
+  routes.delete('/:id',  EmployeeController.delete);
   server.use(prefix, routes);
 };
