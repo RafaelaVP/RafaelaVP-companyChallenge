fd7f40c3bf802fdbdf5a87aeb106f52a9af199e4
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-25 15:16:27 -0300

    reafatory: add new methods crud

--- src/app/service/EmployeeService.js
+++ src/app/service/EmployeeService.js
@@ -5,6 +5,22 @@ class EmployeeService {
     const result = await EmployeeRepository.create(payload);
     return result;
   }
+  async findAll(search) {
+    const result = await EmployeeRepository.findByParams(search);
+    return result;
+  }
+  async update(_id, payload) {
+    return await EmployeeRepository.update({ _id }, payload);
+  }
+
+  async delete(id) {
+     return await EmployeeRepository.delete(id);
+  }
+
+  async getById(id) {
+    const result = await EmployeeRepository.getById(id);
+    return result;
+  }
 }
 
 module.exports = new EmployeeService();
