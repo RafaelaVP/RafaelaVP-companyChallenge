08ef073d28e9fa62e5a0bfd8244767e76da198f3
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-25 15:14:21 -0300

    feat: create update, delete, get all and getby id methods.

--- src/app/repository/BaseRepository.js
+++ src/app/repository/BaseRepository.js
@@ -1,9 +1,24 @@
 class BaseRepository {
-    constructor(schemas) {
-        this._schemas = schemas;
+    constructor(schema) {
+        this._schema = schema;
     }
     async create(payload) {
-        return await this._schemas.create(payload);
+        return await this._schema.create(payload);
     }
+    async findByParams(payload) {
+        const result = await  this.schema.find(payload)
+        return result
+    }
+    async update(_id, payload) {
+        return this._schema.findByIdAndUpdate(_id, payload, { new: true });
+      }
+    
+      async delete(_id) {
+        return this._schema.findByIdAndRemove(_id);
+      }
+    
+      async getById(id) {
+        return this._schema.findById(id);
+      }
 }
  module.exports = BaseRepository;
