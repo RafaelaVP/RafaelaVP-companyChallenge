18d9a1f668d818cd7696ea97f6e24107399d5757
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-26 10:25:08 -0300

    feat: created bad request error

---
+++ src/app/errors/BadRequest.js
@@ -0,0 +1,10 @@
+class BadRequest extends Error {
+    constructor(description) {
+      super();
+      this.statusCode = 400;
+      this.description = description;
+      this.message = 'Bad Request';
+    }
+  }
+  
+  module.exports = BadRequest;
