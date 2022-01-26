cd1b5b9ec5359f8e914014030c131daee44a2f27
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-26 10:15:35 -0300

    refatory: placed in the routes the product

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const employee = require('./employeeRoute');
-
+const product = require('./productRoute');
 module.exports = (server) => {
   server.use((req, res, next) => {
     employee(server, new Router());
+    product(server, new Router());
     next();
   });
 };
