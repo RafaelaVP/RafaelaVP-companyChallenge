1c9d2b8a38b90accbfc08605b40100d3f5c36da3
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-26 12:04:29 -0300

    refatory: verified that it was having a problem due to tar calling the employee in the product route after adjustments is working.

--- src/app/routes/employeeRoute.js
+++ src/app/routes/employeeRoute.js
@@ -3,7 +3,6 @@ const validCreate = require ('../vallidation/employee/createVallidate');
 const validUpdate = require('../vallidation/employee/updateValidate');
 module.exports = (server, routes, prefix = '/api/v1/employee') => {
   routes.post('/',validCreate, EmployeeController.create);
-  routes.get('/:id',  EmployeeController.getById);
   routes.get('/', EmployeeController.getAll);
   routes.put('/:employee_id', validUpdate, EmployeeController.update);
   routes.delete('/:id',  EmployeeController.delete);
--- src/app/routes/productRoute.js
+++ src/app/routes/productRoute.js
@@ -1,7 +1,7 @@
-const EmployeeController = require('../controller/ProductController');
+const ProductController = require('../controller/ProductController');
 
-module.exports = (server, routes, prefix = '/api/v1/employee') => {
-  routes.post('/',validCreate, EmployeeController.create);
-  routes.get('/',  EmployeeController.getAll);
+module.exports = (server, routes, prefix = '/api/v1/product') => {
+  routes.post('/', ProductController.create);
+  routes.get('/',  ProductController.getAll);
   server.use(prefix, routes);
 };
