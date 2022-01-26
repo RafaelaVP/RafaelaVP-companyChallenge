7080284e5a738b36881d7dc42f3785d15fdb9688
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-26 10:21:33 -0300

    feat: created product repository

---
+++ src/app/repository/ProductRepository.js
@@ -0,0 +1,10 @@
+const schemaProduct = require('../schemas/productSchemas');
+const BaseRepository = require('./BaseRepository')
+
+class ProductRepository extends BaseRepository {
+  constructor() {
+    super(schemaProduct);
+  }
+}
+
+module.exports = new ProductRepository();
