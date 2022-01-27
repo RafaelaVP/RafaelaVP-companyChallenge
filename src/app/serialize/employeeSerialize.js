47d2c9d2be287f78c37940470d988ccc9b4b4c1d
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-27 17:44:57 -0300

    refatory: adjusted because it was under the wrong name.

--- src/app/serialize/employeeSerialize.js
+++ src/app/serialize/employeeSerialize.js
@@ -11,7 +11,7 @@ const serialize = ({ _id, name, cpf, birthday, office, situation }) => ({
 });
 
 const paginatedSerialize = ({ docs, totalDocs, pagingCounter, totalPages }) => ({
-  empleyees: docs.map(serialize),
+  employees: docs.map(serialize),
   currentPage: pagingCounter,
   pageSize: docs.length,
   totalCount: totalDocs,
