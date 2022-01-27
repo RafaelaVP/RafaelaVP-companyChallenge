78a25c070cb0f86db272fd5f05bad006459d815b
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-27 17:49:42 -0300

    refatory: placed in find to check the body still testing.

--- src/app/controller/ProductController.js
+++ src/app/controller/ProductController.js
@@ -1,4 +1,5 @@
 const ProductService = require('../service/ProductService');
+const { paginatedSerialize } = require('../serialize/productSerialize');
 
 class ProductController {
   async create(req, res) {
@@ -6,14 +7,24 @@ class ProductController {
       const result = await ProductService.create(req.body);
       return res.status(201).json(result);
     } catch (error) {
-      return res.status(400).json(error.message);
+      return res.status(400).json({
+        message: error.message,
+        details: {
+          description: error.description
+        }
+      });
     }
   }
 
   async getAll(req, res) {
     try {
+      const algumacoisa = {
+        search: req.query,
+        body: req.body
+      };
+      console.log(algumacoisa);
       const result = await ProductService.findAll(req.query);
-      return res.status(200).json(result);
+      return res.status(200).json(paginatedSerialize(result));
     } catch (error) {
       return res.status(400).json(error.message);
     }
