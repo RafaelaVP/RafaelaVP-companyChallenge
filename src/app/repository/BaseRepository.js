146c391bb693353adb277c1fe122272592117eb5
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-27 17:48:34 -0300

    refatory: adjust repository for price lookup

--- src/app/repository/BaseRepository.js
+++ src/app/repository/BaseRepository.js
@@ -10,9 +10,9 @@ class BaseRepository {
 
   async findByParams(search) {
     console.log(search);
-    const { limit = 100, offset = 0, test, ...query } = search;
+    const { limit = 100, offset = 0, ...query } = search;
     return this._schema.paginate(
-      { query, test },
+      query,
 
       { limit, offset }
     );
