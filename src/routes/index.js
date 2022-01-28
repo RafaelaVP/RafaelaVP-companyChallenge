157362cfa8a940d91971cc168dac60a0cc7076c7
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-28 15:25:28 -0300

    feat: created swagger documentation and its respective route.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
 const employee = require('./employeeRoute');
 const product = require('./productRoute');
+const swagger = require('./swaggerRoute');
 
 module.exports = (server) => {
   server.use((req, res, next) => {
     employee(server, new Router());
     product(server, new Router());
+    swagger(server, new Router());
     next();
   });
 };
