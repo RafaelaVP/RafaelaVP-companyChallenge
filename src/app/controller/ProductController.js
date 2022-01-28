5d3f37719c53ce2dfeaf4ca3714e1cac378656ab
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-28 10:54:29 -0300

    refatory: adjusted findall will not need the filter.

--- src/app/controller/ProductController.js
+++ src/app/controller/ProductController.js
@@ -1,11 +1,11 @@
 const ProductService = require('../service/ProductService');
-const { paginatedSerialize } = require('../serialize/productSerialize');
+const { paginatedSerialize, serialize } = require('../serialize/productSerialize');
 
 class ProductController {
   async create(req, res) {
     try {
       const result = await ProductService.create(req.body);
-      return res.status(201).json(result);
+      return res.status(201).json(serialize(result));
     } catch (error) {
       return res.status(400).json({
         message: error.message,
@@ -18,11 +18,6 @@ class ProductController {
 
   async getAll(req, res) {
     try {
-      const algumacoisa = {
-        search: req.query,
-        body: req.body
-      };
-      console.log(algumacoisa);
       const result = await ProductService.findAll(req.query);
       return res.status(200).json(paginatedSerialize(result));
     } catch (error) {
