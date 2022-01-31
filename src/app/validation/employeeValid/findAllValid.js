8ab7c2c52290f367c2f7e4afee1a3bab3d26af54
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-31 09:30:35 -0300

    refatory: add enumOffice in findAll validation.

--- src/app/validation/employeeValid/findAllValid.js
+++ src/app/validation/employeeValid/findAllValid.js
@@ -1,12 +1,15 @@
 const Joi = require('joi').extend(require('@joi/date'));
 const BadRequest = require('../../errors/BadRequest');
+const { enumOffice } = require('../../utils/enum');
 
 module.exports = async (req, res, next) => {
   try {
     const schema = Joi.object({
       name: Joi.string().min(4).max(50),
       cpf: Joi.string().min(11).max(11),
-      office: Joi.string().valid('gerente', 'vendedor', 'caixa')
+      office: Joi.string()
+        .valid(...enumOffice)
+        .trim()
     });
     const { error } = schema.validate(req.query, { abortEarly: false });
     if (error) throw new BadRequest(error.message);
